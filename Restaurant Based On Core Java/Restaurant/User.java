f3cceb39114475ff3323f31fb299ba813a7536f5
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-05-20 12:45:37 +0500

    Add files via upload

---
+++ Restaurant Based On Core Java/Restaurant/User.java
@@ -0,0 +1,72 @@
+package com.ConsoleProject.Restaurant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+abstract public class User {
+    private String name;
+    private String gender;
+    private Order order;
+    private static FoodItem foodItem;
+    User(String name, String gender){
+        this.name = name;
+        this.gender = gender;
+        foodItem = new FoodItem();
+        order = new Order();
+    }
+    public String getGender() {
+        return gender;
+    }
+    public Order getOrder() {
+        return order;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setOrder(String foodKey, Integer foodValue){
+        this.order.setFoodItem(foodKey,foodValue);
+    }
+
+    public void getFastFood() {
+        System.out.println(" ~ ~ ~ ~ ~ Fast Food  ~ ~ ~ ~ ~ ");
+        this.getFoodItems(foodItem.getFastFood());
+
+        //        foodItem.getFastFood().forEach((food, price) -> System.out.println("Food Items: "+food+ " , Price: " +price+ " Rs."));
+    }
+    public void getDesiFood() {
+        System.out.println(" ~ ~ ~ ~ ~ Desi Food  ~ ~ ~ ~ ~ ");
+        this.getFoodItems(foodItem.getDesiFood());
+//        foodItem.getDesiFood().forEach((food, price) -> System.out.println("Food Items: "+food+ " , Price: " +price+ " Rs."));
+    }
+    private void getFoodItems(Map<String, Integer> fooditem) {
+        int i = 0;
+        for (Map.Entry mapFood : fooditem.entrySet()){
+            System.out.println("Item "+ (++i) +". : "+mapFood.getKey()+ " , Price: " +mapFood.getValue()+ " Rs.");
+        }
+    }
+
+}
+
+class Customer extends User{
+    private int bill;
+    Customer(String name, String gender){
+        super(name, gender);
+        this.bill = 0;
+    }
+
+    public void setBill(int bill) {
+        this.bill = bill;
+    }
+
+    public int getBill() {
+        return bill;
+    }
+}
+class Admin extends User{
+
+    Admin(String name, String gender) {
+        super(name, gender);
+    }
+}
