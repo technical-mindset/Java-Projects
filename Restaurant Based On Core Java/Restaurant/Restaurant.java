fd9dd203ba111d9607ea5985ce9fc77259f1176a
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-05-25 16:03:24 +0500

    Update Restaurant.java

--- Restaurant Based On Core Java/Restaurant/Restaurant.java
+++ Restaurant Based On Core Java/Restaurant/Restaurant.java
@@ -33,6 +33,75 @@ public class Restaurant {
 
          }
 
+}
 
+ private static void addCustomer(){
+        Customer customer;
+        System.out.println("  ---- Enter Name: ------");
+        String name = scanner.next();
+
+        System.out.println("  ---- Enter Gender: ------");
+        String gender = scanner.next();
+
+        customer = new Customer(name,gender);
+        customers.add(displayFood(customer));
+         totalBill(customer);
+
+
+    }
+    private static Customer displayFood(Customer customer){
+        System.out.println("Please enter '1' for \"Fast Food\" or '2' for \"Desi Food\"");
+        System.out.print("--> ");
+        // for number on food list
+        int index = 0;
+        int input = scanner.nextInt();
+
+        if (input == 1) {
+            System.out.println("   ^^^^^^^   Fast Food   ^^^^^^   ");
+
+           for (Map.Entry foodItem: fi.getFastFood().entrySet()) {
+                System.out.print((++index) +". Food Items: "+foodItem.getKey()+ " , Price: " +foodItem.getValue()+ " Rs.");
+
+                    //total spaces are 19
+                    for (int i = 0; i < (19 - ((foodItem.getKey().toString().length() + foodItem.getValue().toString().length()) - 11)); i++) {
+                        System.out.print(" ");
+
+                    }
+                if (index % 2 == 0) {
+                    System.out.println();
+                }
+            }
+
+                customer = selection_of_order(customer,fi.getFastFood());
+
+
+        }
+        else if (input == 2) {
+            System.out.println("   ++++++   Desi Food   ++++++   ");
+//            fi.getDesiFood().forEach((food, price) -> System.out.println("Food Items: "+food+ " , Price: " +price+ " Rs."));
+
+           for (Map.Entry foodItem: fi.getDesiFood().entrySet()) {
+                System.out.print((++index) +". Food Items: "+foodItem.getKey()+ " , Price: " +foodItem.getValue()+ " Rs.");
+
+                    //total spaces are 19
+                    for (int i = 0; i < (19 - ((foodItem.getKey().toString().length() + foodItem.getValue().toString().length()) - 11)); i++) {
+                        System.out.print(" ");
+                    }
+
+                if (index % 2 == 0) {
+                    System.out.println();
+                }
+            }
+            customer = selection_of_order(customer,fi.getDesiFood());
+        }
+        else if (input >= 3 || input < 0) {
+            System.out.println("\033[0;1m" +"Invalid Insertion!");
+            customer = displayFood(customer);
+        }
+
+        //
+
+        return (customer);
+    }
 
 }
