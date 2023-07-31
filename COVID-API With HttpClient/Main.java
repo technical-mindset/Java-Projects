68058f7e5fc0d20fe07362f5ecf02d511e124ac8
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-07-31 15:57:28 +0500

    Update Main.java

--- COVID-API With HttpClient/Main.java
+++ COVID-API With HttpClient/Main.java
@@ -9,20 +9,6 @@ public class Main {
         // Task 1
        CovidApi.getData();
 
-       // Task 2
-    try{
-        Connection con = DB_Connection.getInstance().getConnection();
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM employees;");
 
-        while(rs.next()){
-            System.out.println(rs.getString(1) +"     "+rs.getString(2)+"     "+
-                    rs.getString(3)+"    "+rs.getString(4));
-        }
-        System.out.println(rs);
-    }
-    catch (Exception e){
-        e.printStackTrace();
-    }
     }
 }
